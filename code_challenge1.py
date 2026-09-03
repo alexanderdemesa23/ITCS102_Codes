e9cd540df3a3d8dbeecd65f386cd04e990b2a4d8
alexanderdemesa23 <alexanderdemesa23@gmail.com>
2026-09-03 11:29:43 +0800

    Codes for ITCS102

---
+++ code_challenge1.py
@@ -0,0 +1,4 @@
+
+STAR = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t*\t\tJOHN ALEXANDER V. DE MESA\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+print(STAR)
