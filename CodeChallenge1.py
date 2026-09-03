4f031d4595360e394da9977636dff6c5cbffbc58
alexanderdemesa23 <alexanderdemesa23@gmail.com>
2026-09-03 11:29:14 +0800

    Delete CodeChallenge1.py

--- CodeChallenge1.py
+++
@@ -1,4 +0,0 @@
-
-STAR = "\t\t\t\t\t\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\tJOHN ALEXANDER V. DE MESA\n\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*"
-
-print(STAR)
